ce3f4b1503de0a38b4f873dbb02e4fab65f09ffa
rogeralsing <rogeralsing@gmail.com>
2014-12-01 23:28:58 +0100

    biz

--- EF6 DDD Intro/EFDDD/Domain/Person.cs
+++ EF6 DDD Intro/EFDDD/Domain/Person.cs
@@ -1,4 +1,6 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity.Infrastructure;
 using System.Diagnostics;
 
 namespace EFDDD.Domain
@@ -22,5 +24,21 @@ namespace EFDDD.Domain
 
         public PersonalName PersonalName { get; private set; }
         public Address Address { get; private set; }
+
+        public void Relocate(Address relocateToAddress)
+        {
+            if (Address == relocateToAddress)
+                throw new ArgumentException("The new address must not be equal to the current address","relocateToAddress");
+
+            Address = relocateToAddress;
+        }
+
+        public void Rename(PersonalName newName)
+        {
+            if (PersonalName == newName)
+                throw new ArgumentException("The new name must not be equal to the current name","newName");
+
+            PersonalName = newName;
+        }
     }
 }
